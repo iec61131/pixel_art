1896fc7a3914a5e2946474a5b6cb38c0515544c8
Tobias Roeder <Tobias.Roeder@rohde-schwarz.com>
2023-11-13 20:05:48 -0500

    create pixel art dist

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='pixel_art',
+    version='0.1',
+    description='blab lbab',
+    author='mein name',
+    packages=['pixel_art'],
+    install_requires = ['colorama']
+
+)
